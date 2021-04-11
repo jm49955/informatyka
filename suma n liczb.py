c08b6ca3114bf2d7aa6fffe9a4e2fd2508443d65
jm49955 <82313028+jm49955@users.noreply.github.com>
2021-04-11 12:52:28 +0200

    Add files via upload

---
+++ suma n liczb.py
@@ -0,0 +1,15 @@
+N = 37
+
+kwota = 1000.00
+
+rata = kwota/N
+
+konto = 0.00
+
+for i in range(N):
+    konto = konto + rata
+    
+    print()
+    print('powinno być =', kwota) 
+    print('stan konta  ='  konto)
+    
